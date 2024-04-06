778b916bf7274063152e6c53d760cb4d36f0c1ab
Egydio Bolonhezi Neto <egydiobolonhezi@gmail.com>
2024-04-06 14:00:00 -0300

    fix (#1)
    * fix
    
    * fix
    
    ---------
    
    Co-authored-by: EgydioBNeto <egydio.neto@tmaior.com.br>

--- dist/routes/check-in.mjs
+++
@@ -1,8 +0,0 @@
-import {
-  checkIn
-} from "../chunk-DJJN76QW.mjs";
-import "../chunk-JRO4E4TH.mjs";
-import "../chunk-JV6GRE7Y.mjs";
-export {
-  checkIn
-};
--- dist/routes/register-for-event.mjs
+++
@@ -1,8 +0,0 @@
-import {
-  registerForEvent
-} from "../chunk-DH5BZUFF.mjs";
-import "../chunk-JRO4E4TH.mjs";
-import "../chunk-JV6GRE7Y.mjs";
-export {
-  registerForEvent
-};
